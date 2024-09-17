ee2e3c01e7f6d5742c9e95883125f5743e34dc57
Tyler Wu <tylerwu2222@gmail.com>
2024-09-17 12:44:04 -0700

    trying with lib prisma

--- src/app/api/prisma/birds/route.ts
+++ src/app/api/prisma/birds/route.ts
@@ -1,10 +1,10 @@
 import { NextResponse } from "next/server";
-import { PrismaClient } from "@prisma/client";
+import { prisma } from '@/app/lib/prisma';
 
-const prisma = new PrismaClient();
+// const prisma = new PrismaClient();
 
 export async function GET() {
     const birds = await prisma.birds.findMany();
     // console.log('birds res', birds);
-    return NextResponse.json(birds, {status: 200})
+    return NextResponse.json(birds, { status: 200 })
 }
