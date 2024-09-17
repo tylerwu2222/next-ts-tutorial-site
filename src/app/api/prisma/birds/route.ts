0d389278a8fe29b3ebd69daaf4c74b20cb767be6
Tyler Wu <tylerwu2222@gmail.com>
2024-09-17 12:52:11 -0700

    ran prisma generate

--- src/app/api/prisma/birds/route.ts
+++ src/app/api/prisma/birds/route.ts
@@ -1,8 +1,6 @@
 import { NextResponse } from "next/server";
 import { prisma } from '@/app/lib/prisma';
 
-// const prisma = new PrismaClient();
-
 export async function GET() {
     const birds = await prisma.birds.findMany();
     // console.log('birds res', birds);
